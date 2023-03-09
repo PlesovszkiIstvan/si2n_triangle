31d8182a14ac054d5b5dd7737081a86fd11a346d
Plesovszki István <kilot58@gmail.com>
2023-03-09 10:29:25 +0100

    About javítása

---
+++ src/app.js
@@ -0,0 +1,29 @@
+/*
+* File: app.js
+* Author: Plesovszki István
+* Copyright: 2023, Plesovszki István
+* Group: Szoft I-2-N
+* Date: 2023-03-09
+* Github: https://github.com/PlesovszkiIstvan/
+* Licenc: GNU GPL
+*/
+
+var baseInput = document.querySelector('#base');
+var heightInput = document.querySelector('#height');
+var areaInput = document.querySelector('#area');
+var calcButton = document.querySelector('#calcButton');
+
+calcButton.addEventListener('click', () => {
+    workInput();
+});
+
+function workInput() {
+    let base = Number(baseInput.value);
+    let height = Number(heightInput.value);
+    let area = calcTriangleArea(base,height);
+    areaInput.value = area;
+}
+
+function calcTriangleArea(base, height) {
+    return base * height / 2;
+}
